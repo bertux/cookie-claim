2f41555b9a78afdfb437da64ce65f1c50f785da4
Bertrand Juglas <b@juglas.name>
2025-05-11 19:59:36 +0200

    Initial version from Gitcoin (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,43 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import { type BaseError, useWaitForTransactionReceipt, useWriteContract } from 'wagmi'
-import { abi } from './abi'
-import { parseEther } from 'viem'
+import ErrorBoundary from "@/components/ErrorBoundary";
+import { CookieJarInterface } from "@/components/WithdrawalInterface";
+import { CONTRACT_ADDRESS } from "@/config/contract";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CookieJarPublicInfo } from "./components/Claim";
+import { WithdrawalHistory } from "./components/WithdrawalHistory";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error:errorConnect } = useConnect()
-  const { disconnect } = useDisconnect()
-  const { data: hash, error, isPending, writeContract } = useWriteContract()
-  const address = '0x69D349E2009Af35206EFc3937BaD6817424729F7'
-
-  async function deposit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const formData = new FormData(e.target as HTMLFormElement)
-    const amount = formData.get('amountWrap') as string
-    writeContract({
-      address: address,
-      abi,
-      functionName: 'deposit',
-      value: parseEther(amount),
-    })
-  }
-
-  async function withdraw(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const formData = new FormData(e.target as HTMLFormElement)
-    const amount = formData.get('amountUnwrap') as string
-    writeContract({
-      address: address,
-      abi,
-      functionName: 'withdraw',
-      args: [parseEther(amount)],
-    })
-  }
-
-  const { isLoading: isConfirming, isSuccess: isConfirmed } =
-    useWaitForTransactionReceipt({
-      hash,
-    })
-
   return (
-    <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <div className="min-h-screen bg-gray-100 py-8">
+          <div className="container mx-auto px-4">
+            <div className="mb-4 flex justify-end">
+              <ConnectButton />
+            </div>
+            <CookieJarPublicInfo contractAddress={CONTRACT_ADDRESS} />
+            <CookieJarInterface contractAddress={CONTRACT_ADDRESS} />
+            <WithdrawalHistory contractAddress={CONTRACT_ADDRESS} />
+          </div>
+          <footer className="flex items-center justify-center py-4 mt-10">
+            <img src="/allo.jpg" alt="Logo" className="h-8 mr-2" />
+            <span>
+              Powered by{" "}
+              <a
+                target="_blank"
+                href="https://allo.capital"
+                className="text-blue-500"
+              >
+                allo.capital
+              </a>
+            </span>
+          </footer>
         </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{errorConnect?.message}</div>
-      </div>
-
-      <div>
-        <h2>Wrap</h2>
-        <form onSubmit={deposit}>
-          <input name="amountWrap" type="number" defaultValue="0.1" step="0.000000000000000001" required />
-          <button disabled={isPending} type="submit">{isPending ? 'Confirming...' : 'Wrap'}</button>
-        </form>
-      </div>
-
-      <div>
-        <h2>Unwrap</h2>
-        <form onSubmit={withdraw}>
-          <input name="amountUnwrap" type="number" defaultValue="0.1" step="0.000000000000000001" required />
-          <button disabled={isPending} type="submit">{isPending ? 'Confirming...' : 'Unwrap'}</button>
-        </form>
-      </div>
-
-      <div>
-        <h2>Status</h2>
-        {hash && <div>Transaction Hash: {hash}</div>}
-          {isConfirming && <div>Waiting for confirmation...</div>}
-          {isConfirmed && <div>Transaction confirmed.</div>}
-          {error && (
-            <div>Error: {(error as BaseError).message || error.message}</div>
-          )}
-      </div>
-    </>
-  )
+      </ErrorBoundary>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
